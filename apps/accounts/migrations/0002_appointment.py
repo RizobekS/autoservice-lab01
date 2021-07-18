2ba0f0d085a0f5e6f4f9501c5717f387e707e568
Alkamyan David <ada_170301@mail.ru>
2021-07-18 19:57:30 +0500

    resolve migration issues

--- apps/accounts/migrations/0002_appointment.py
+++ apps/accounts/migrations/0002_appointment.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('site_settings', '0006_auto_20210709_0149'),
+        ('site_settings', '0001_initial'),
         ('accounts', '0001_initial'),
     ]
 
