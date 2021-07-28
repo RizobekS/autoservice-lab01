909aae80cb0f110307bf8d02f8e798f3c499eb1c
Alkamyan David <ada_170301@mail.ru>
2021-07-28 15:15:13 +0500

    updated gallery urls

--- apps/work_gallery/urls.py
+++ apps/work_gallery/urls.py
@@ -5,6 +5,6 @@ from apps.work_gallery.views import WorkGalleryView, SingleWorkView
 app_name = 'work_gallery'
 
 urlpatterns = [
-    path('works/', WorkGalleryView.as_view(), name='list'),
-    path('work/<str:work_url>/', SingleWorkView.as_view(), name='single'),
+    path('', WorkGalleryView.as_view(), name='list'),
+    path('<str:work_url>/', SingleWorkView.as_view(), name='single'),
 ]
