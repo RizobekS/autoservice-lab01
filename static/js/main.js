f59d4a43c2819181426be88f3ca6c8b741297a9f
Alkamyan David <ada_170301@mail.ru>
2021-09-17 09:50:27 +0500

    changes for main slider at index page

--- static/js/main.js
+++ static/js/main.js
@@ -397,74 +397,160 @@
                     searchControlProvider: 'yandex#search'
                 }),
 
-                // РЎРѕР·РґР°С‘Рј РјР°РєРµС‚ СЃРѕРґРµСЂР¶РёРјРѕРіРѕ.
+                // Создаём макет содержимого.
                 // MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
                 //     '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
                 // ),
 
                 myPlacemark_01 = new ymaps.Placemark([60.00960156407456, 30.32629749999995], {
                     iconContent: iconContent,
-                    hintContent: 'Р­РЅРіРµР»СЊСЃР°, 33/1',
-                    // balloonContent: 'РђРІС‚РѕРџСЂРµРјРёСѓРј'
+                    hintContent: 'Энгельса, 33/1',
+                    // balloonContent: 'АвтоПремиум'
                 }, {
-                    // РћРїС†РёРё.
-                    // РќРµРѕР±С…РѕРґРёРјРѕ СѓРєР°Р·Р°С‚СЊ РґР°РЅРЅС‹Р№ С‚РёРї РјР°РєРµС‚Р°.
+                    // Опции.
+                    // Необходимо указать данный тип макета.
                     iconLayout: 'default#imageWithContent',
                     // iconLayout: circleLayout,
-                    // РЎРІРѕС‘ РёР·РѕР±СЂР°Р¶РµРЅРёРµ РёРєРѕРЅРєРё РјРµС‚РєРё.
+                    // Своё изображение иконки метки.
                     iconImageHref: '',
-                    // Р Р°Р·РјРµСЂС‹ РјРµС‚РєРё.
+                    // Размеры метки.
                     // iconShape: {
                     //     type: 'Circle',
-                    //     // РљСЂСѓРі РѕРїРёСЃС‹РІР°РµС‚СЃСЏ РІ РІРёРґРµ С†РµРЅС‚СЂР° Рё СЂР°РґРёСѓСЃР°
+                    //     // Круг описывается в виде центра и радиуса
                     //     coordinates: [0, 0],
                     //     radius: 30
                     // }
                     iconImageSize: iconSize,
                     iconContentSize: iconSize,
-                    // РЎРјРµС‰РµРЅРёРµ Р»РµРІРѕРіРѕ РІРµСЂС…РЅРµРіРѕ СѓРіР»Р° РёРєРѕРЅРєРё РѕС‚РЅРѕСЃРёС‚РµР»СЊРЅРѕ
-                    // РµС‘ "РЅРѕР¶РєРё" (С‚РѕС‡РєРё РїСЂРёРІСЏР·РєРё).
+                    // Смещение левого верхнего угла иконки относительно
+                    // её "ножки" (точки привязки).
                     // iconImageOffset: [-12, -14]
                 }),
 
                 myPlacemark_02 = new ymaps.Placemark([59.93665943024874, 30.479307499999983], {
                     iconContent: iconContent,
-                    hintContent: 'РҐР°СЃР°РЅСЃРєР°СЏ, 5',
-                    // balloonContent: 'РђРІС‚РѕРџСЂРµРјРёСѓРј'
+                    hintContent: 'Хасанская, 5',
+                    // balloonContent: 'АвтоПремиум'
                 }, {
-                    // РћРїС†РёРё.
-                    // РќРµРѕР±С…РѕРґРёРјРѕ СѓРєР°Р·Р°С‚СЊ РґР°РЅРЅС‹Р№ С‚РёРї РјР°РєРµС‚Р°.
+                    // Опции.
+                    // Необходимо указать данный тип макета.
                     iconLayout: 'default#imageWithContent',
-                    // РЎРІРѕС‘ РёР·РѕР±СЂР°Р¶РµРЅРёРµ РёРєРѕРЅРєРё РјРµС‚РєРё.
+                    // Своё изображение иконки метки.
                     iconImageHref: '',
-                    // Р Р°Р·РјРµСЂС‹ РјРµС‚РєРё.
+                    // Размеры метки.
                     iconImageSize: iconSize,
                     iconContentSize: iconSize,
-                    // РЎРјРµС‰РµРЅРёРµ Р»РµРІРѕРіРѕ РІРµСЂС…РЅРµРіРѕ СѓРіР»Р° РёРєРѕРЅРєРё РѕС‚РЅРѕСЃРёС‚РµР»СЊРЅРѕ
-                    // РµС‘ "РЅРѕР¶РєРё" (С‚РѕС‡РєРё РїСЂРёРІСЏР·РєРё).
+                    // Смещение левого верхнего угла иконки относительно
+                    // её "ножки" (точки привязки).
                     // iconImageOffset: [-12, -14]
                 });
 
             myMap.geoObjects.add(myPlacemark_01);
             myMap.geoObjects.add(myPlacemark_02);
         });
+
     }
 
+// function initGoogleMap() {
+//         //Google Map script
+//         var $googleMaps = $('#map, .page_map');
+//         if ( $googleMaps.length ) {
+//             $googleMaps.each(function() {
+//                 var $map = $(this);
 
-    function inputsMask() {
-        var elements = document.querySelectorAll('input[name="phone"]');
-        var maskOptions = {
-            mask: '+{7} (000) 000 - 00 - 00',
-            lazy: false,
-            placeholderChar: '_'
-        };
-        for (var i = 0; i < elements.length; i++) {
-            var mask = IMask(elements[i], maskOptions);
-        }
-    }
+//                 var lat;
+//                 var lng;
+//                 var map;
+
+//                 //map styles. You can grab different styles on https://snazzymaps.com/
+
+//                 //dark style
+//                 //var styles = [{"featureType":"all","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#c4c4c4"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#707070"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21},{"visibility":"on"}]},{"featureType":"poi.business","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#be2026"},{"lightness":"0"},{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels.text.stroke","stylers":[{"visibility":"off"},{"hue":"#ff000a"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#575757"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.text.stroke","stylers":[{"color":"#2c2c2c"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#999999"}]},{"featureType":"road.local","elementType":"labels.text.stroke","stylers":[{"saturation":"-52"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
+
+//                 // light style
+//                 var styles = [{"featureType": "water","elementType": "geometry","stylers": [{"color": "#e9e9e9"},{"lightness": 17}]},{"featureType": "landscape","elementType": "geometry","stylers": [{"color": "#f5f5f5"},{"lightness": 20}]},{"featureType": "road.highway","elementType": "geometry.fill","stylers": [{"color": "#ffffff"},{"lightness": 17}]},{"featureType": "road.highway","elementType": "geometry.stroke","stylers": [{"color": "#ffffff"},{"lightness": 29},{"weight": 0.2}]},{"featureType": "road.arterial","elementType": "geometry","stylers": [{"color": "#ffffff"},{"lightness": 18}]},{"featureType": "road.local","elementType": "geometry","stylers": [{"color": "#ffffff"},{"lightness": 16}]},{"featureType": "poi","elementType": "geometry","stylers": [{"color": "#f5f5f5"},{"lightness": 21}]},{"featureType": "poi.park","elementType": "geometry","stylers": [{"color": "#dedede"},{"lightness": 21}]},{"elementType": "labels.text.stroke","stylers": [{"visibility": "on"},{"color": "#ffffff"},{"lightness": 16}]},{"elementType": "labels.text.fill","stylers": [{"saturation": 36},{"color": "#333333"},{"lightness": 40}]},{"elementType": "labels.icon","stylers": [{"visibility": "off"}]},{"featureType": "transit","elementType": "geometry","stylers": [{"color": "#f2f2f2"},{"lightness": 19}]},{"featureType": "administrative","elementType": "geometry.fill","stylers": [{"color": "#fefefe"},{"lightness": 20}]},{"featureType": "administrative","elementType": "geometry.stroke","stylers": [{"color": "#fefefe"},{"lightness": 17},{"weight": 1.2}]}];
+
+//                 //markers
+//                 var $markers = $map.find('.marker');
+
+//                 //map settings
+//                 var address = $markers.first().find('.marker-address').text() ? $markers.first().find('.marker-address').text() : 'london, baker street, 221b';
+//                 var geocoder = new google.maps.Geocoder();
 
 
+//                 var draggable = $map.data('draggable') ? $map.data('draggable') : false;
+//                 var scrollwheel = $map.data('scrollwheel') ? $map.data('scrollwheel') : false;
 
+//                 //type your address after "address="
+//                 geocoder.geocode({
+//                     address: address
+//                 }, function(data){
+
+//                     lat = data[0].geometry.location.lat();
+//                     lng = data[0].geometry.location.lng();
+
+//                     var center = new google.maps.LatLng(lat, lng);
+//                     var settings = {
+//                         mapTypeId: google.maps.MapTypeId.ROADMAP,
+//                         zoom: 16,
+//                         draggable: draggable,
+//                         scrollwheel: scrollwheel,
+//                         center: center,
+//                         styles: styles
+//                     };
+//                     map = new google.maps.Map($map[0], settings);
+
+//                     var infoWindows = [];
+
+//                     $($markers).each(function(index) {
+
+//                         var $marker = $(this);
+//                         var markerTitle = $marker.find('.marker-title').text();
+
+//                         //building info widnow HTML code
+//                         var markerDescription = '';
+//                         markerDescription += markerTitle ? '<h3 class="makret-title">' + markerTitle + '</h3>' : '';
+//                         markerDescription += $marker.find('.marker-description').html() ? '<div class="marker-description">' + $marker.find('.marker-description').html() + '</div>' : '';
+//                         if(markerDescription) {
+//                             markerDescription = '<div class="map_marker_description">' + markerDescription + '</div>';
+//                         }
+
+//                         geocoder.geocode({
+//                             address: $marker.find('.marker-address').text()
+//                         }, function(data){
+//                             var iconSrc = $marker.find('.marker-icon').attr('src');
+
+//                             var lat = data[0].geometry.location.lat();
+//                             var lng = data[0].geometry.location.lng();
+
+//                             var center = new google.maps.LatLng(lat, lng);
+
+//                             var marker = new google.maps.Marker({
+//                                 position: center,
+//                                 title: markerTitle,
+//                                 map: map,
+//                                 icon: iconSrc
+//                             });
+
+//                             var infowindow = new google.maps.InfoWindow({
+//                                 content: markerDescription
+//                             });
+
+//                             infoWindows.push(infowindow);
+
+//                             google.maps.event.addListener(marker, 'click', function() {
+//                                 for (var i=0;i<infoWindows.length;i++) {
+//                                     infoWindows[i].close();
+//                                 }
+//                                 infowindow.open(map,marker);
+//                             });
+//                         });
+//                     });
+//                 });
+//             }); //each Google map
+//         }//google map length
+//     }
+//     window.initGoogleMap=initGoogleMap;
 
 
 //function that initiating template plugins on window.load event
@@ -965,8 +1051,6 @@
             });
         }
 
-        inputsMask();
-
 
     }//documentReadyInit
 
@@ -1045,6 +1129,31 @@
                     smoothHeight: false,
                     slideshowSpeed: 5000,
                     animationSpeed: 800,
+                })
+            });
+        }
+
+        if (document.documentElement.clientWidth <= 320) {
+            $(".choose-slider").each(function (index) {
+                console.log('choose-slider slider #' + index);
+                var $currentSlider = $(this);
+                console.log($currentSlider);
+                console.log($currentSlider.find('.flex-active-slide').length);
+                if ($currentSlider.find('.flex-active-slide').length) {
+                    return;
+                }
+                console.log('$currentSlider.flexslider');
+                $currentSlider.flexslider({
+                    animation: 'fade',
+                    useCSS: true,
+                    pauseOnHover: true,
+                    controlNav: true,
+                    directionNav: false,
+                    prevText: "",
+                    nextText: "",
+                    smoothHeight: false,
+                    slideshow: true,
+                    slideshowSpeed: 5000,
                 })
             });
         }
