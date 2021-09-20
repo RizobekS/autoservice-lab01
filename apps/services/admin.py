8ee9961e93839a502dbf5641b0cb14a5dfab2758
Alkamyan David <ada_170301@mail.ru>
2021-09-20 18:53:01 +0500

    minor changes

--- apps/services/admin.py
+++ apps/services/admin.py
@@ -75,10 +75,10 @@ class ProductAdmin(ImageCroppingMixin, SortableAdmin):
             'fields': (('title', 'url'), 'active', 'show_at_homepage', 'tag', 'section', ('price', 'fixed_price'), 'time_duration', 'spare_parts', 'cars', 'similar_products')
         }),
         ('Текст', {'fields': ('short_description', 'description'), 'classes': ('wide',)}),
+        ('Главная страница (Список акций)', {'fields': ('show_in_promotions', 'homepage_description'), 'classes': ['wide', 'collapse']}),
         ('Изображения', {
             'fields': ('image', 'thumbnail_1960x600', 'thumbnail_960x585', 'thumbnail_455x200', 'thumbnail_348x236', 'thumbnail_268x118', 'thumbnail_80x80')
         }),
-        ('Главная страница (Список акций', {'fields': ('show_in_promotions', 'homepage_description'), 'classes': ['wide', 'collapse']}),
         ('CEO настройки', {'fields': ('canonical_to_original', 'meta_title', 'meta_header', 'meta_description', 'meta_keywords', 'meta_robots'), 'classes': ['wide', 'collapse']})
     )
 
