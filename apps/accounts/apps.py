b79205cd6454f96a05b6a4629ba165b0414a9aee
Alkamyan David <ada_170301@mail.ru>
2021-10-06 11:47:06 +0500

    Minor changes

--- apps/accounts/apps.py
+++ apps/accounts/apps.py
@@ -5,4 +5,4 @@ class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.accounts'
 
-    verbose_name = 'Аккаунты'
+    verbose_name = 'Аккаунты и заявки'
