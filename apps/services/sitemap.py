1bcb3758a7c8e77fc8adcd8f2ef2ce8aee3e9531
Alkamyan David <ada_170301@mail.ru>
2021-11-02 22:44:48 +0500

    Minor changes & upgrades

--- apps/services/sitemap.py
+++ apps/services/sitemap.py
@@ -1,6 +1,7 @@
 from typing import Dict, Union
 
 from django.contrib.sitemaps import Sitemap
+from django.db import OperationalError
 from django.urls import reverse
 
 from apps.services.models import Product, Section
@@ -31,21 +32,25 @@ def service_sitemaps() -> Dict[str, type]:
         Returns dictionary {<product-slug>: <sitemap-class-for-this-product>}
         Caches computed url components into cached_car_packs dict, which has separate key for each car pack and None for car_pack with all cars included
     """
-    cached_car_packs = {}  # All cached car_packs are stored here (item which includes all cars is also)
-
     sitemaps = {}
 
-    for section in Section.objects.filter(active=True, canonical_to_original=False):
-        if None not in cached_car_packs:  # Sections support all cars, so car_pack is always None
-            cached_car_packs[None] = create_cached_car_url_list(None)
+    try:
+        cached_car_packs = {}  # All cached car_packs are stored here (item which includes all cars is also)
 
-        sitemaps[section.url] = _sitemap_factory(section, cached_car_packs[None])
+        for section in Section.objects.filter(active=True, canonical_to_original=False):
+            if None not in cached_car_packs:  # Sections support all cars, so car_pack is always None
+                cached_car_packs[None] = create_cached_car_url_list(None)
 
-    for product in Product.objects.filter(active=True, canonical_to_original=False):
+            sitemaps[section.url] = _sitemap_factory(section, cached_car_packs[None])
 
-        if product.car_pack not in cached_car_packs:  # Create cached car pack for the product's car pack if does not exist
-            cached_car_packs[product.car_pack] = create_cached_car_url_list(product.car_pack)
+        for product in Product.objects.filter(active=True, canonical_to_original=False):
 
-        sitemaps[product.url] = _sitemap_factory(product, cached_car_packs[product.car_pack])
+            if product.car_pack not in cached_car_packs:  # Create cached car pack for the product's car pack if does not exist
+                cached_car_packs[product.car_pack] = create_cached_car_url_list(product.car_pack)
 
+            sitemaps[product.url] = _sitemap_factory(product, cached_car_packs[product.car_pack])
+
+    except OperationalError:
+        print('Could not generate sitemap classes for Section and/or Product')
+        print('Ignore this message if you running "makemigrations" or "migrate"')
     return sitemaps
