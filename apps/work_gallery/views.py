8d0313122f670e959355de3eebe2a6173a4df8d4
Alkamyan David <ada_170301@mail.ru>
2021-12-29 06:21:53 +0500

    OpenGraph for work_gallery entries

--- apps/work_gallery/views.py
+++ apps/work_gallery/views.py
@@ -7,6 +7,8 @@ from apps.work_gallery.utils.mixins import CategoriesMixin
 from utils.breadcrumbs.types import Breadcrumb
 from utils.breadcrumbs.utils import reverse_bc
 from utils.mixins import PageSettingsMixin
+from utils.opengraph import OpengraphMixin
+from utils.opengraph.utils import og_thumbnail
 
 
 class WorkGalleryView(ListView, CategoriesMixin, PageSettingsMixin):
@@ -19,7 +21,7 @@ class WorkGalleryView(ListView, CategoriesMixin, PageSettingsMixin):
     viewname = 'work_gallery:list'
 
 
-class SingleWorkView(DetailView, PageSettingsMixin):
+class SingleWorkView(DetailView, PageSettingsMixin, OpengraphMixin):
     template_name = 'work_gallery/work.html'
     queryset = Work.objects.filter(active=True)
     slug_field = 'url'
@@ -28,6 +30,17 @@ class SingleWorkView(DetailView, PageSettingsMixin):
 
     viewname = 'work_gallery:single'
     initial_breadcrumbs = [reverse_bc(view=WorkGalleryView)]
+
+    def get_og_tags(self, **kwargs) -> dict:
+        meta_context = super().as_context()
+
+        kwargs.update({
+            'og:title': meta_context['page_title'],
+            'og:description': meta_context['meta_description'],
+            **og_thumbnail(self.request, self.object.image_set.first(), 'page_thumbnail'),
+            'og:url': self.request.build_absolute_uri(self.request.path),
+        })
+        return super().get_og_tags(**kwargs)
 
     def get_current_breadcrumb(self):
         return [Breadcrumb(self.object.title, '#')]
